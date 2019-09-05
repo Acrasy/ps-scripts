8534e33cc7ab97c0795fed77a15f5ae818018638
Acrasy <sai.whocares@gmail.com>
2019-09-05 11:48:30 +0200

    script to set certain appointemnts to private
    type category to set to private, if set to public, replace sensitivity=2 to sensitivity=0

---
+++ markCategoryAsPrivate.ps1
@@ -0,0 +1,12 @@
+﻿# Outlook Connection
+$Outlook = New-Object -ComObject Outlook.Application
+#Connect to Outlook Calendar
+$OutlookCalendar = $Outlook.session.GetDefaultFolder(9) #Value 9 for calendar entry/ value 10 for contacts
+
+$category = Read-Host -Promt "enter category"
+foreach($i in $OutlookCalendar.items){
+    if($i.Categories -like   "*$category*"){              #Items contain $subject
+        $i.Sensitivity=2;                               #Sensitivity=2  makes private
+        $i.Save();                                      #write changes to server
+    }
+}
